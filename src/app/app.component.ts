ccb63466a544d92f8fb3729fb1c0f7a4a9015f0d
nirbheek <nirbheekb12@gmail.com>
2018-10-08 11:13:09 +0530

    Dropdown Directive added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-angular-app';
+ loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+
+  }
+
 }
