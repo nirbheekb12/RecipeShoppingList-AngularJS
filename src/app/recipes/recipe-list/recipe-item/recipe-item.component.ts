eb260398382006ded9eb87e31d24702afccf311b
nirbheek <nirbheekb12@gmail.com>
2018-10-18 16:28:42 +0530

    Recipe Edit Component Added Routes Added

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,6 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Recipe} from '../../recipe.model';
-import {RecipeService} from '../../recipe.service';
+
 
 @Component({
   selector: 'app-recipe-item',
@@ -10,15 +10,8 @@ import {RecipeService} from '../../recipe.service';
 export class RecipeItemComponent implements OnInit {
   // bind property from outside
     @Input() recipe: Recipe;
-
-  constructor(private recipeService: RecipeService) { }
+    @Input() index: number;
 
   ngOnInit() {
   }
-
-  onSelected() {
-        this.recipeService.recipeSelected.emit(this.recipe);
-
-  }
-
 }
